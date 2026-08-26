589d1cf671a7da9d177e926a8094bac7d111e43e
mohammad mahdi sadeghi <mohammad12345sadeghi@gmail.com>
2026-08-26 23:37:16 +0330

    Compose home page sections inline in app/page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,33 @@
-import Home from "../src/views/Home";
+import Hero from "../../src/pages/Home/sections/Hero";
+import Stats from "../../src/pages/Home/sections/Stats";
+import DualSchool from "../../src/pages/Home/sections/DualSchool";
+import Story from "../../src/pages/Home/sections/Story";
+import Pillars from "../../src/pages/Home/sections/Pillars";
+import Ecosystem from "../../src/pages/Home/sections/Ecosystem";
+import EventsCarousel from "../../src/pages/Home/sections/EventsCarousel";
+import Faq from "../../src/pages/Home/sections/Faq";
+import Honors from "../../src/pages/Home/sections/Honors";
+import Rokadians from "../../src/pages/Home/sections/Rokadians";
+import Comments from "../../src/pages/Home/sections/Comments";
+import Blogs from "../../src/pages/Home/sections/Blogs";
+import FinalCTA from "../../src/pages/Home/sections/FinalCTA";
 
-export default function Page() {
-  return <Home />;
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Stats />
+      <DualSchool />
+      <Story />
+      <Pillars />
+      <Ecosystem />
+      <EventsCarousel />
+      <Faq />
+      <Honors />
+      <Rokadians />
+      <Comments />
+      <Blogs />
+      <FinalCTA />
+    </>
+  );
 }

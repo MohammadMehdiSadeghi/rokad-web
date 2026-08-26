03c06eafcb9581ca2aa0633c2a2c67b82b4bc69d
mohammad mahdi sadeghi <mohammad12345sadeghi@gmail.com>
2026-08-26 23:44:17 +0330

    set next

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,17 @@
-import Hero from "../../src/pages/Home/sections/Hero";
-import Stats from "../../src/pages/Home/sections/Stats";
-import DualSchool from "../../src/pages/Home/sections/DualSchool";
-import Story from "../../src/pages/Home/sections/Story";
-import Pillars from "../../src/pages/Home/sections/Pillars";
-import Ecosystem from "../../src/pages/Home/sections/Ecosystem";
-import EventsCarousel from "../../src/pages/Home/sections/EventsCarousel";
-import Faq from "../../src/pages/Home/sections/Faq";
-import Honors from "../../src/pages/Home/sections/Honors";
-import Rokadians from "../../src/pages/Home/sections/Rokadians";
-import Comments from "../../src/pages/Home/sections/Comments";
-import Blogs from "../../src/pages/Home/sections/Blogs";
-import FinalCTA from "../../src/pages/Home/sections/FinalCTA";
+import Hero from "../../src/views/Home/sections/Hero";
+import Stats from "../../src/views/Home/sections/Stats";
+import DualSchool from "../../src/views/Home/sections/DualSchool";
+import Story from "../../src/views/Home/sections/Story";
+import Pillars from "../../src/views/Home/sections/Pillars";
+import Ecosystem from "../../src/views/Home/sections/Ecosystem";
+import EventsCarousel from "../../src/views/Home/sections/EventsCarousel";
+import Faq from "../../src/views/Home/sections/Faq";
+import Honors from "../../src/views/Home/sections/Honors";
+import Rokadians from "../../src/views/Home/sections/Rokadians";
+import Comments from "../../src/views/Home/sections/Comments";
+import Blogs from "../../src/views/Home/sections/Blogs";
+import FinalCTA from "../../src/views/Home/sections/FinalCTA";
+import RokadHierarchy from "../../src/views/Home/sections/RokadHierarchy";
 
 export default function Home() {
   return (
@@ -21,6 +22,7 @@ export default function Home() {
       <Story />
       <Pillars />
       <Ecosystem />
+      <RokadHierarchy />
       <EventsCarousel />
       <Faq />
       <Honors />

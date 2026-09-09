7fa154f80db1299e5bcedf624780b1b656c75b7f
mohammad mahdi sadeghi <mohammad12345sadeghi@gmail.com>
2026-09-10 02:26:55 +0330

    style(home): Fallah photo +5px lower

--- src/views/Home/sections/TeamTeaser/index.jsx
+++ src/views/Home/sections/TeamTeaser/index.jsx
@@ -75,7 +75,7 @@ const MEMBERS = [
     color: "#E0195B",
     bgLight: "bg-[#FEFAFB]",
     pattern: patternPink,
-    imgClass: "translate-y-[1.15rem] sm:translate-y-[1.3rem] lg:translate-y-[1.55rem] scale-[1.4]",
+    imgClass: "translate-y-[1.45rem] sm:translate-y-[1.6rem] lg:translate-y-[1.85rem] scale-[1.4]",
   },
   {
     name: "علیرضا عزیزپور",
